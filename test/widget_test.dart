fd01f5be9fc0b321ccf91c44620648d464d8e574
Shadrach Aroni <shadracking7@gmail.com>
2025-09-29 14:14:48 +0300

    Corrections

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:SmartSync/main.dart';
+import 'package:smartsync/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

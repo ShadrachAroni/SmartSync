b176d1901fe0b7f6e318f2a0b0889da3db2ae998
Shadrach Aroni <shadracking7@gmail.com>
2025-10-29 14:02:19 +0300

    changes

--- app/ios/Flutter/flutter_export_environment.sh
+++ app/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\USER\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\USER\Desktop\new_app"
+export "FLUTTER_APPLICATION_PATH=C:\Users\USER\Documents\Course work\year 4 sem 2\ICS Project II\Final Project\SmartSync\app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
